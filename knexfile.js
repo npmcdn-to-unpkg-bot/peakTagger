c8a84f8cf4b1067c603f2dfd8dd4a190b307c756
Ian Strouse <ian.strouse@gmail.com>
2016-08-28 17:19:20 -0600

    Fix bug

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
-module.exports = {
+var dotenv = require('dotenv');
+dotenv.load();
 
+module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost:5432/peaktagger'
   },
-  testing: {
+  development: {
     client: 'pg',
     connection: process.env.DATABASE_URL
   }
