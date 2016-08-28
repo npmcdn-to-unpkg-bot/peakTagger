4b97577b07281c2a1ce0ca80152af7082d82e5a9
Ian Strouse <ian.strouse@gmail.com>
2016-08-28 17:08:41 -0600

    Add heroku database

--- knexfile.js
+++ knexfile.js
@@ -3,5 +3,9 @@ module.exports = {
   development: {
     client: 'postgres',
     connection: 'postgres://localhost:5432/peaktagger'
+  },
+  testing: {
+    client: 'postgres',
+    connection: process.env.DATABASE_URL
   }
 };
