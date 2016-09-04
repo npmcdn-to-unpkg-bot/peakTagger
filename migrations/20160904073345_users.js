af62a20c01ac810fa6f66f29bafe80f1be3bf6a5
Ian Strouse <ian.strouse@gmail.com>
2016-09-04 07:46:33 -0600

    Add users table

---
+++ migrations/20160904073345_users.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments('id').primary();
+    table.string('username').unique().notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.boolean('isAdmin').notNullable().defaultTo(false);
+    table.boolean('useMetric').notNullable().defaultTo(false);
+    table.boolean('ptPoints').notNullable().defaultTo(0);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
